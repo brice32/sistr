a78087ec31719d3e825e51ab9cdbbdc004f9be57
brice32 <wangyc32@hotmail.com>
2016-11-28 01:02:11 +0100

    26.2 bis
    保存没弄好

--- framework/F3il.php
+++ framework/F3il.php
@@ -1,3 +1,8 @@
 <?php
+namespace F3il;
 define('F3IL','');
+defined('APPLICATION_PATH') or die('erreu APPLICATION_PATH dans f3il');
+defined('APPLICATION_NAMESPACE') or die('erreu APPLICATION_NAMESPACE dans f3il');
 require_once '/framework/page.php';
+require_once '/framework/application.php';
+require_once '/framework/configuration.php';
